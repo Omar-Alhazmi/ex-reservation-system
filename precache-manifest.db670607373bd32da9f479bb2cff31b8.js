2c6429b7b9a6835f25fdf06554f3489c7e3c4dac
OMA <vex.xcc@gmail.com>
2022-10-27 00:59:27 +0300

    Updates

---
+++ precache-manifest.db670607373bd32da9f479bb2cff31b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "209929dd8de33de79b7b",
+    "url": "/static/css/main.780681dc.chunk.css"
+  },
+  {
+    "revision": "209929dd8de33de79b7b",
+    "url": "/static/js/main.209929dd.chunk.js"
+  },
+  {
+    "revision": "7991d2d16e47c8443d47",
+    "url": "/static/js/1.7991d2d1.chunk.js"
+  },
+  {
+    "revision": "ee7feb5837f202945d71",
+    "url": "/static/js/2.ee7feb58.chunk.js"
+  },
+  {
+    "revision": "42927bd64711a8b15ff8",
+    "url": "/static/js/runtime~main.42927bd6.js"
+  },
+  {
+    "revision": "f9be1ee0d09ac16ae1ad865a1427b478",
+    "url": "/static/media/logo.f9be1ee0.png"
+  },
+  {
+    "revision": "b42992d138cb52a175fb17f1790b0395",
+    "url": "/index.html"
+  }
+];
