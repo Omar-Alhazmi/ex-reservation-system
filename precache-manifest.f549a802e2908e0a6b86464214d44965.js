0bfd30158842830470ff30701571fafe58add7ed
OMA <vex.xcc@gmail.com>
2022-10-29 17:56:08 +0300

    Updates

---
+++ precache-manifest.f549a802e2908e0a6b86464214d44965.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db3b411f38a66c1fa519",
+    "url": "/ex-reservation-system/static/css/main.1ea6f7f0.chunk.css"
+  },
+  {
+    "revision": "db3b411f38a66c1fa519",
+    "url": "/ex-reservation-system/static/js/main.db3b411f.chunk.js"
+  },
+  {
+    "revision": "255667dbea0b73219cea",
+    "url": "/ex-reservation-system/static/js/1.255667db.chunk.js"
+  },
+  {
+    "revision": "9f5c94c8af0c185783fb",
+    "url": "/ex-reservation-system/static/js/2.9f5c94c8.chunk.js"
+  },
+  {
+    "revision": "ef45b950076b88210543",
+    "url": "/ex-reservation-system/static/js/runtime~main.ef45b950.js"
+  },
+  {
+    "revision": "f9be1ee0d09ac16ae1ad865a1427b478",
+    "url": "/ex-reservation-system/static/media/logo.f9be1ee0.png"
+  },
+  {
+    "revision": "edca59ca9087fdc7c9716cdd69cbb156",
+    "url": "/ex-reservation-system/index.html"
+  }
+];
